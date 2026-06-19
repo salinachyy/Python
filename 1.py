1bf60ec4355893f534bdee1a98c3c2a3b6de287c
chysalina <chysalina6315@gmail.com>
2026-06-19 06:51:28 +0545

    Python code

---
+++ 1.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+a="bca4"
+print(type(a))
+# _a="apple"
+# a=5 integer
+# a=12.5 float
+# a="Salina" string
+b='''"+-*%'@"'''
+print(b)
+c="ball,apple"
+d=c.split(",")
+print(d[0])
+import random
+inp=input("Enter your Decision:")
+val=["Scissor","Paper","Rock"]
+Computer=random.choice(val)
+print(Computer)
